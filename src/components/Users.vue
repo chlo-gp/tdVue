15d02aed342d6d475fea5828775e1a2ccef30d97
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-01-28 18:40:12 +0100

    search users

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -29,6 +29,11 @@
     <div id="divHidden">
       {{ genderFilter }}
     </div>
+    <input
+      v-model="searchUsers"
+      type="text"
+      placeholder="Rechercher un user"
+    >
     <table
       id="tbl-users"
       class="table table-hover"
@@ -39,15 +44,31 @@
           <th>Nom</th>
           <th>Email</th>
           <th>Tel</th>
-          <th>Genre</th>
-          <th>Age</th>
+          <th>Genre  </th>
+          <th>
+            Age <svg
+              id="sort-users"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              @click="sortUsers"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
+              />
+            </svg>
+          </th>
         </tr>
       </thead>
       <tbody
         id="tbody-users"
       >
         <tr
-          v-for="user in usersFiltered"
+          v-for="user in searchedUsers"
           :key="user.email"
         >
           <td><img :src="user.picture.thumbnail"></td>
@@ -55,7 +76,7 @@
           <td>{{ user.email }}</td>
           <td>{{ user.phone }}</td>
           <td>{{ user.gender }}</td>
-          <td>{{ user.dob.age }}</td>
+          <td>{{ user.dob.age }} </td>
         </tr>
       </tbody>
     </table>
@@ -71,12 +92,19 @@ export default {
     return {
       results: [],
       errored: false,
-      genderFilter: ['male', 'female']
+      genderFilter: ['male', 'female'],
+      searchUsers:''
     }
   },
   computed: {
     usersFiltered() {
       return this.results.filter(user => this.genderFilter.includes(user.gender))
+    },
+    searchedUsers() {
+      return this.usersFiltered.filter((user) => {
+          return (user.name.first+' '+user.name.last).match(new RegExp(this.searchUsers, 'i'));
+
+      })
     }
   },
   methods: {
@@ -89,6 +117,13 @@ export default {
             this.errored = true
           })
     },
+    sortUsers: function () {
+      console.log("fgbv");
+      return this.usersFiltered.sort((user) => {
+        return user.dob.age.;
+
+      });
+    }
   },
 }
 </script>
